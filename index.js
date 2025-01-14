02e2b57c4a6b5c03a19f6a56ad9da2674fbb53e4
Mahajabin Tokon <tokonislam1122@gmail.com>
2025-01-13 19:12:32 -0700

    get api to get single product

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5001;
@@ -11,18 +11,18 @@ app.use(cors());
 app.use(express.json());
 // Verify Token middleware
 const verifyToken = (req, res, next) => {
-    const auth = req.headers.authorization;
-    // console.log("Inside", auth);
-    if (!auth) return res.status(401).send({ message: "forbidden access" });
-    const token = auth.split(" ")[1];
-    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
-      if (err) {
-        return res.status(401).send({ message: "forbidden access" });
-      }
-      req.decoded = decoded;
-      next();
-    });
-  };
+  const auth = req.headers.authorization;
+  // console.log("Inside", auth);
+  if (!auth) return res.status(401).send({ message: "forbidden access" });
+  const token = auth.split(" ")[1];
+  jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.85wcl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -68,10 +68,18 @@ async function run() {
 
     // Product collection related routes
     // Get all products
-    app.get("/products", verifyToken, async (req, res) => {
+    app.get("/products", async (req, res) => {
       const result = await productCollection.find().toArray();
       res.send(result);
     });
+
+    // Get product by ID
+    app.get("/productDetails/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await productCollection.findOne(query);
+        res.send(result);
+      });
 
     // Add a new product
     app.post("/addProduct", async (req, res) => {
